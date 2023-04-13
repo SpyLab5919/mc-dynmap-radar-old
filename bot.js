ea9a0542faf7751712e9c65b53bca4af69ae0a9b
SpyLab5919 <northvarvar@gmail.com>
2023-04-13 12:04:30 +0300

    fix

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import openDb from './db.js';
-import {Telegraf} from 'telegraf';
-import {chromium} from 'playwright';
+import { Telegraf } from 'telegraf';
+import { chromium } from 'playwright';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -17,7 +17,7 @@ await page.setViewportSize({ width: 1200, height: 700 });
 
 try {
   await page.goto(url);
-} catch(err) {
+} catch (err) {
   console.log(`[${new Date().toISOString()}] Помилка підключення до динмапи`);
   console.log(err);
   ctx.reply(`Помилка підключення до динмапи`);
@@ -25,34 +25,40 @@ try {
 
 
 await bot.launch().catch((err) => console.error(err));
-console. log("bot.launch");
+console.log("bot.launch");
 
 bot.use(async (ctx, next) => {
   //if (ctx.update.from === undefined) return; // 
   const userId = ctx.update.message?.from.id;
   const chatId = ctx.update.message?.chat.id;
   const update = ctx.update.message || ctx.update.callback_query;
-// userId == process.env.RIGHTFUL_USER_ID || 
+  // userId == process.env.RIGHTFUL_USER_ID || 
 
+  if (ctx.update.message?.text == '/terminate') {
+    await ctx.reply(`Вимикаюсь...`);
 
-try {
-  if (chatId == process.env.MAIN_CHAT_ID) {
-    console.log(`[${new Date().toISOString()}] ${ctx.update.message?.from.username} ${userId} використав ${ctx.update.message?.text}`);
-    await next();
-    return;
+    bot.stop('SIGNINT');
+    process.exit();
   }
-  
-  if (userId != process.env.RIGHTFUL_USER_ID || chatId != process.env.MAIN_CHAT_ID) {
-    await next();
-    console.log(`[${new Date().toISOString()}] ${ctx.update.message?.from.username} ${userId} використав ${ctx.update.message?.text} у ${ctx.update.message?.chat.username} ${chatId}`);
-    return;
-  }
-  await ctx.reply('У вас немає прав.');
-} catch (err) {
+  try {
+    if (chatId == process.env.MAIN_CHAT_ID) {
+      console.log(`[${new Date().toISOString()}] ${ctx.update.message?.from.username} ${userId} використав ${ctx.update.message?.text}`);
+      await next();
+      return;
+    }
+
+    if (userId != process.env.RIGHTFUL_USER_ID || chatId != process.env.MAIN_CHAT_ID) {
+      await next();
+      console.log(`[${new Date().toISOString()}] ${ctx.update.message?.from.username} ${userId} використав ${ctx.update.message?.text} у ${ctx.update.message?.chat.username} ${chatId}`);
+      return;
+    }
+    await ctx.reply('У вас немає прав.');
+  } catch (err) {
     console.error(err);
-    bot.reply(`Виникла помилка при виконанні програми`);
-}
+    ctx.reply(`Виникла помилка при виконанні програми`);
+  }
 
+  
 });
 
 bot.command('seewhitelist', (ctx) => {
@@ -76,13 +82,13 @@ bot.command('seewhitelist', (ctx) => {
 
 bot.command('addplayer', (ctx) => {
   const playerName = ctx.update.message.text.split(' ')[1];
-  
+
   if (playerName === undefined) {
     ctx.reply(`Введіть нік гравця`);
     return;
   }
   const user = ctx.update.message.from.username;
-  
+
   console.log(`[${new Date().toISOString()}] ${user} додав ${playerName} до вайтлісту.`);
   ctx.reply(`Додаю ${playerName} до вайтлісту.`);
   whitelist.push(playerName);
@@ -91,12 +97,12 @@ bot.command('addplayer', (ctx) => {
 
 bot.command('removeplayer', (ctx) => {
   const playerName = ctx.update.message.text.split(' ')[1];
-  
+
   if (playerName === undefined) {
     ctx.reply(`Введіть нік гравця`);
     return;
   }
-  
+
   const user = ctx.update.message.from.username;
   const playerIndex = whitelist.indexOf(playerName);
 
@@ -129,10 +135,10 @@ bot.command('screenshot', async (ctx) => {
   await bot.telegram.sendChatAction(ctx.chat.id, 'typing');
 
   const screenshot = await page.screenshot({
-    clip: {x: 250, y: 0, width: 700, height: 700}
+    clip: { x: 250, y: 0, width: 700, height: 700 }
   }).catch((err) => {
     console.error(err);
-    bot.reply(`Виникла помилка при скріншоті`);
+    ctx.reply(`Виникла помилка при скріншоті`);
     return;
   });
 
@@ -142,11 +148,7 @@ bot.command('screenshot', async (ctx) => {
   console.log(`[${new Date().toISOString()}] Скріншот мапи`);
 });
 
-bot.command('terminate', async (ctx) => {
-	await ctx.reply(`Вимикаюсь..,`);
-	bot.stop('SIGNINT');
-  process.exit(10);
-})
+
 
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
